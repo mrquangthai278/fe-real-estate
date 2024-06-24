a85561e856364a8af3dd348173e2ab84b3c22d9a
mrquangthai278 <mrquangthai278@gmail.com>
2024-06-25 01:56:36 +0700

    Refactor table component

--- pages/dashboard/operator/list.vue
+++ pages/dashboard/operator/list.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="h-full flex-1 flex-col space-y-8">
-    <DataTable :data="tasks" :columns="columns" />
+    <DataTable :data="tasks" :columns="columnsTest" />
   </div>
 </template>
 
 <script setup lang="ts">
 import tasks from "@/config/dashboard/operator/mock.json";
-import { columns } from "@/config/dashboard/operator/columns";
+import { columns } from "@/config/dashboard/operator/columns.tsx";
 
-import DataTable from "@/components/block/TableView/components";
+import DataTable from "@/components/block/TableView/components/index.vue";
 
 definePageMeta({
   layout: "dashboard",
 });
+
+const columnsTest = columns()
 </script>
